fea99a831447053323b2066e60ac18f85968e653
Anilmal <baranovart231999@gmail.com>
2018-04-03 19:16:26 +0300

    Переделал класс

--- students/Baranov_A/task4/Lab4.cpp
+++ students/Baranov_A/task4/Lab4.cpp
@@ -22,6 +22,13 @@ struct Observation
 		weight = obj.weight;
 		return *this;
 	}
+	void SetObservation(int dt_day, int dt_month, int dt_year, int weight_)
+	{
+		nabl.date_day = dt_day;
+		nabl.date_month = dt_month;
+		nabl.date_year = dt_year;
+		weight = weight_;
+	}
 	//Перегрузка оператора вывода
 	friend ostream& operator<<(ostream& os, Observation& obj);
 };
@@ -45,6 +52,12 @@ struct Date
 		return *this;
 	}
 	friend ostream& operator<<(ostream& os, Date& obj);
+	void Setdate(int dt_day, int dt_month, int dt_year)
+	{
+		date_day = dt_day;
+		date_month = dt_month;
+		date_year = dt_year;
+	}
 };
 //перегрузка оператора вывода для структуры Date
 ostream& operator<<(ostream& os, Date& obj)
@@ -82,74 +95,45 @@ private:
 	int count;
 	int min;
 	int max;
+	int mid;
+	int mid_of_month;
 	Date StartDate;
 public:
 	//Конструктор
 	FloorWeight()
 	{
 		people = 0;
-		count = 0;
-
 	}
 	//Конструктор по параметрам
-	FloorWeight(int _peop, int _tim)
+	FloorWeight(int _peop)
 	{
 		people = _peop;
-		count = _tim;
-		int j = 0;
-		Observation Nab{ 0,0 };//Пустая структура чтобы заполнить наблюдения
 		Family = new Person[people];
-		for (int i = 0;i < people;i++)
-		{
-			while (j != count)
-			{
-				Family[i].zamer.push_back(Nab);
-				j++;
-			}
-		}
 	};
 	//Деструктор
 	~FloorWeight()
 	{
 		delete[] Family;
 	}
-	//Установка даты для всего числа наблюдений
-	void SetDate(vector <Observation> Fam_, int _peo, int cnt_)
-	{
-		people = _peo;
-		count = cnt_;
-		Observation Nab{ 0,0 };
-		int a = 0;
-		Family = new Person[people];
-		for (int i = 0;i < people;i++)
-		{
-			while (a != count)
-			{
-				Family[i].zamer.push_back(Nab);
-				a++;
-			}
-			for (int j = 0;j < count;j++)
-			{
-				Family[i].zamer[j].nabl.date_day = Fam_[j].nabl.date_day;
-				Family[i].zamer[j].nabl.date_month = Fam_[j].nabl.date_month;
-				Family[i].zamer[j].nabl.date_year = Fam_[j].nabl.date_year;
-			}
-		}
-	}
 	//Устанавливает наблюдение для выбранного человека
 	void SetNabl(Observation nabl, int number)
 	{
-				Family[number-1].Name = getchar();//задаю имя взвешенного человека
-				Family[number-1].zamer.push_back(nabl);//вношу данные взвешивания
+		for (int i = 0;i < people;i++)
+		{
+			if (i == number)
+			{
+				Family[i].Name = getchar();
+				Family[i].zamer.push_back(nabl);
+			}
+		}
 
 	}
 	//установка имен
-	void SetName(string *Names)
+	void ChangeName(int number, string Nam)
 	{
-		for (int i = 0;i < people;i++)
-		{
-			Family[i].Name = Names[i];
-		}
+
+		Family[number - 1].Name = Nam;
+
 	}
 	//Установка первой даты (относится исключительно  к классу
 	void Setfirstdate(Date Fd)
@@ -157,17 +141,6 @@ public:
 
 		StartDate = Fd;
 
-	}
-	//Установка веса для всего числа наблюдений
-	void SetWeight(vector <Observation> Fam_)
-	{
-		for (int i = 0;i < people;i++)
-		{
-			for (int j = 0;j < count;j++)
-			{
-				Family[i].zamer[j].weight = Fam_[j].weight;
-			}
-		}
 	}
 	//Получение веса в выбранный день для выбранного человека
 	int Getweight(int number, int dt_day, int dt_mounth, int dt_year)
@@ -196,16 +169,13 @@ public:
 	//Получить минимальный вес для выбранного человека
 	int Minweight(int number)
 	{
-		for (int i = 0; i < people; i++)
+
+		for (int j = 1; j < count; j++)
 		{
-			if (i == number)
+			min = Family[number].zamer[0].weight;
+			if (Family[number].zamer[j].weight < min)
 			{
-				for (int j = 1; j < count; j++)
-				{
-					min = Family[i].zamer[0].weight;
-					if (Family[i].zamer[j].weight < min)
-						min = Family[i].zamer[j].weight;
-				}
+				min = Family[number].zamer[j].weight;
 			}
 		}
 		return min;
@@ -214,19 +184,52 @@ public:
 	int Maxweight(int number)
 	{
 
-		for (int i = 0; i < people; i++)
+		for (int j = 1; j < count; j++)
 		{
-			if (i == number)
+			max = Family[number].zamer[0].weight;
+			if (Family[number].zamer[j].weight > max)
 			{
-				for (int j = 1; j < count; j++)
-				{
-					max = Family[i].zamer[0].weight;
-					if (Family[i].zamer[j].weight > max)
-						max = Family[i].zamer[j].weight;
-				}
+				max = Family[number].zamer[j].weight;
 			}
 		}
 		return max;
+	}
+	//Получить средний вес человека за все время наблюдений
+	int Midweight(string Nam)
+	{
+		for (int i = 0;i < people;i++)
+		{
+			if (Family[i].Name == Nam)
+			{
+				for (int j = 0;j <= Family[i].zamer.size();j++)
+				{
+					mid += Family[i].zamer[j].weight;
+					count++;
+				}
+				mid = mid / count;
+				return mid;
+			}
+		}
+	}
+	//Получить средний вес человека в выбранном месяце
+	int Midweightofmonth(string Nam, int dt_month)
+	{
+		for (int i = 0;i < people;i++)
+		{
+			if (Family[i].Name == Nam)
+			{
+				for (int j = 0;j <= Family[i].zamer.size();j++)
+				{
+					if (Family[i].zamer[j].nabl.date_month == dt_month)
+					{
+						mid_of_month += Family[i].zamer[j].weight;
+						count++;
+					}
+				}
+				mid_of_month = mid_of_month / count;
+				return mid_of_month;
+			}
+		}
 	}
 	//Запись в файл результатов
 	void writetofile()
@@ -270,7 +273,7 @@ public:
 		}
 	}
 	//Добавить члена семьи
-	void addfam(string Name, vector <Observation> Fm)
+	void addfam(string Name, Observation Fm)
 	{
 		people += 1;//увеличиваем праметр колличества людей
 		Person *NewFamily;//создаем временный ммассив в который запише старые данные и новые
@@ -287,13 +290,11 @@ public:
 			}
 		}
 		NewFamily[people - 1].Name = Name;
-		for (int j = 0;j < count;j++)
-		{
-			Family[people - 1].zamer[j].weight = Fm[j].weight;
-			Family[people - 1].zamer[j].nabl.date_day = Fm[j].nabl.date_day;
-			Family[people - 1].zamer[j].nabl.date_month = Fm[j].nabl.date_month;
-			Family[people - 1].zamer[j].nabl.date_year = Fm[j].nabl.date_year;
-		}
+
+		NewFamily[people - 1].zamer[0].weight = Fm.weight;
+		NewFamily[people - 1].zamer[0].nabl.date_day = Fm.nabl.date_day;
+		NewFamily[people - 1].zamer[0].nabl.date_month = Fm.nabl.date_month;
+		NewFamily[people - 1].zamer[0].nabl.date_year = Fm.nabl.date_year;
 		delete[] Family;//очищаем старый массив
 		Family = new Person[people];//задаем ему нужный размер
 		for (int k = 0;k < people;k++)
@@ -310,122 +311,120 @@ public:
 		}
 	}
 };
-//К сожаленью пока не стал делать мэин(жду ваших замечаний по классу) 
-/*int main()
+int main()
 {
-setlocale(LC_ALL, "rus");
-Observation *k;
-string *Names;
-int people;
-int count;
-FloorWeight K;
-Date Fd;
-int dt_day;
-int dt_month;
-int dt_year;
-int weight_;
-int choice;
-cout << "Введите колличество взвешенных людей: ";
-cin >> people;
-cout << endl;
-cout << "Введите колличество взвешиваний: ";
-cin >> count;
-system("cls");
-Names=new string[people];
-k=new Observation[count];
-for (int i = 0;i < people;i++)
-{
-cout << "Введите имя: ";
-getline(cin,Names[i]);
-cout<< endl;
-for (int j = 0;j < count;j++)
-{
+	setlocale(LC_ALL, "rus");
+	Observation k;
+	string Name;
+	int people;
+	int count;
+	int num;
+	FloorWeight K;
+	Date Fd;
+	int dt_day;
+	int dt_month;
+	int dt_year;
+	int weight_;
+	int choice;
+	int ch;
+	cout << "Введите колличество взвешенных людей: ";
+	cin >> people;
+	cout << endl;
+	system("cls");
+	for (int i = 0;i < people;i++)
+	{
+		cout << "Задайте наблюдение" << endl;
+		cout << "Введите день:";cin >> dt_day;cout << endl;
+		cout << "Введите месяц:";cin >> dt_month;cout << endl;
+		cout << "Введите год:";cin >> dt_year;cout << endl;
+		cout << "Введите вес:";cin >> weight_;cout << endl;
+		k.SetObservation(dt_day, dt_month, dt_year, weight_);
+		K.SetNabl(k, i);
+	}
+	cout << "Что желаете сделать дальше?"
+		<< "1-установить первую дату"
+		<< "2-узнать первую дату"
+		<< "3-узнать для выбранного члена семьи средний результат взвешивания "
+		<< "4-узнать минимальный вес "
+		<< "5-узнать максимальный вес"
+		<< "6-записать все данные в текстовый файл"
+		<< "7-записать данные из в файла в класс"
+		<< "8-Добавить члена семьи"
+		<< "9-выход";
+	switch (choice)
+	{
+	case 1:
+	{
+		cout << "Введите день:";cin >> dt_day;cout << endl;
+		cout << "Введите месяц:";cin >> dt_month;cout << endl;
+		cout << "Введите год:";cin >> dt_year;cout << endl;
+		Fd.Setdate(dt_day, dt_month, dt_year);
+		K.Setfirstdate(Fd);
+	}
+	case 2:
+	{
+		cout << K.Getfirstdate() << endl;
+	}
+	case 3:
+	{
+		cout << "1-За все время наблюдений,2- в выбранный месяц" << endl;
+		cin >> ch;
+		if (ch == 2)
+		{
+			cout << "Введите имя:";cin >> Name;cout << endl;
+			cout << "Введите месяц:";cin >> dt_month;cout << endl;
+			system("cls");
+			cout << K.Midweightofmonth(Name, dt_month);
+			system("pause");
 
-cout << "Введите день:";cin >> dt_day;cout << endl;
-cout << "Введите месяц:";cin >> dt_month;cout << endl;
-cout << "Введите год:";cin >> dt_year;cout << endl;
-cout << "Введите вес:";cin >> weight_;cout << endl;
-k[i].SetNabl(dt_day, dt_month, dt_year, weight_);
-system("cls");
+		}
+		if (ch = 1)
+		{
+			cout << "Введите имя:";cin >> Name;cout << endl;
+			system("cls");
+			cout << K.Midweight(Name);
+			system("pause");
+		}
+	}
+	case 4:
+	{
+		cout << "Введите порядковый номер члена семьи";cin >> num;
+		cout << K.Minweight(num) << endl;
+		system("pause");
+	}
+	case 5:
+	{
+		cout << "Введите порядковый номер члена семьи";cin >> num;
+		cout << K.Maxweight(num) << endl;
+		system("pause");
+	}
+	case 6:
+	{
+		K.writetofile();
+	}
+	case 7:
+	{
+		K.writefromfile();
+	}
+	case 8:
+	{
+		Observation Fm;
+		string Name;
+		cout << "Введите имя: ";
+		getline(cin, Name);
+		cout << endl;
+		cout << "Введите день:";cin >> dt_day;cout << endl;
+		cout << "Введите месяц:";cin >> dt_month;cout << endl;
+		cout << "Введите год:";cin >> dt_year;cout << endl;
+		cout << "Введите вес:";cin >> weight_;cout << endl;
+		Fm.SetObservation(dt_day, dt_month, dt_year, weight_);
+		K.addfam(Name, Fm);
+	}
+	case 9:
+	{
+		return 0;
+	}
+	}
+	system("pause");
+	return 0;
 }
-}
-K.SetDate(k, 1, 2);
-K.SetName(Names);
-K.SetWeight(k);
-cout << "Что желаете сделать дальше?"
-<< "1-установить новую первую дату"
-<< "2-узнать первую дату"
-<< "3-узнать результаты взвешивания в выбранную дату"
-<< "4-узнать минимальный вес "
-<< "5-узнать максимальный вес"
-<< "6-записать все данные в текстовый файл"
-<< "7-записать данные из в файла в класс"
-<< "8-Добавить члена семьи"
-<< "9-выход";
-switch (choice)
-{
-case 1:
-{
-cout << "Введите день:";cin >> dt_day;cout << endl;
-cout << "Введите месяц:";cin >> dt_month;cout << endl;
-cout << "Введите год:";cin >> dt_year;cout << endl;
-Fd.Setdate(dt_day, dt_month, dt_year);
-K.Setfirstdate(Fd);
-}
-case 2:
-{
-cout << K.Getfirstdate(Fd)<<endl;
-}
-case 3:
-{
-cout << "Введите день:";cin >> dt_day;cout << endl;
-cout << "Введите месяц:";cin >> dt_month;cout << endl;
-cout << "Введите год:";cin >> dt_year;cout << endl;
-system("cls");
-cout << K.Getweight(dt_day, dt_month, dt_year)<<endl;
-system("pause");
-}
-case 4:
-{
-cout << K.Minweight()<<endl;
-system("pause");
-}
-case 5:
-{
-cout << K.Maxweight()<<endl;
-system("pause");
-}
-case 6:
-{
-K.writetofile();
-}
-case 7:
-{
-K.writefromfile();
-}
-case 8:
-{
-Observation *Fm;
-string Name;
-cout << "Введите имя: ";
-getline(cin, Name);
-cout << endl;
-Fm = new Observation[count];
-for (int j = 0;j < count;j++)
-{
-cout << "Введите день:";cin >> dt_day;cout << endl;
-cout << "Введите месяц:";cin >> dt_month;cout << endl;
-cout << "Введите год:";cin >> dt_year;cout << endl;
-cout << "Введите вес:";cin >> weight_;cout << endl;
-Fm[j].SetNabl(dt_day, dt_month, dt_year, weight_);
-}
-K.addfam(Name, Fm);
-}
-case 9:
-{
-return 0;
-}
-}
-system("pause");
-return 0;
-}*/
