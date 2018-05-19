fa024632defbdaf0c0535b80fb03403522ccac32
Anilmal <baranovart231999@gmail.com>
2018-05-19 23:02:35 +0300

    v 1.06

--- students/Baranov_A/task6/labwork6.cpp
+++ students/Baranov_A/task6/labwork6.cpp
@@ -121,26 +121,20 @@ public:
 	}
 	void SaveRecords(int steps)
 	{
-		Records[11] = steps;
-		int left = 0;
-		int right = 10;
-		while (left < right)
+		for (int i = 11;i > 0;i--)
 		{
-			while ((Records[right] >= steps) && (left < right))
-				right--;
-			if (left != right)
+			if (Records[i] < steps && Records[i - 1]>steps)
 			{
-				Records[left] = Records[right];
-				left++;
+				Records[i] = steps;
 			}
-			while ((Records[left] <= steps) && (left < right))
-				left++;
-			if (left != right)
+			if (Records[i] > steps)
 			{
-				Records[right] = Records[left];
-				right--;
+				break;
 			}
-
+			if (Records[1] < steps)
+			{
+				Records[1] = steps;
+			}
 		}
 	};
 	void ShowRecords()
